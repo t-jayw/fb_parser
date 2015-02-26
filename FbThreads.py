93c9124d9e849b9f0c427c5b9abd3a641ce5ceef
Tyler Wood <tylerjaywood@gmail.com>
2015-02-25 18:23:11 -0800

    need to update tokenizer to handle short words/ punctuation

--- FbThreads.py
+++ FbThreads.py
@@ -2,7 +2,23 @@ import nltk
 from bs4 import BeautifulSoup as bs
 
 stubfile = "/Users/tylerw/msgsample.htm"
-fullfile = "/Users/tylerw/Downloads/facebook-tylerjaywood/html/messages.htm"
+fullfile = "/Users/tylerw/Downloads/facebook-tylerjaywoodd/html/messages.htm"
+
+
+
+def tokenizeString(string):
+    tokenized = nltk.word_tokenize(string)
+    return tokenized
+
+def removeStopWords(wordlist):
+    goWords = [word.lower() for word in wordlist if word.lower()
+                not in nltk.corpus.stopwords.words('english')]
+    return goWords
+
+def prepString4Nltk(string):
+    tokenized = tokenizeString(string)
+    words = removeStopWords(tokenized)
+    return words
 
 
 class Thread(object):
@@ -43,43 +59,55 @@ class Archive(object):
             [thread.aggregateMessages() for thread in self.threadObjects]
 
 
-    def pullPersonMessages(self, *members):
+
+class PeopleArchive(Archive):
+    def __init__(self, filepath, *members):
+        super(PeopleArchive, self).__init__(filepath)
+        self.members = [name for name in members]
+
+    def pullPersonMessages(self):
         """
         Finds threads with all of members
         returns a dict of {member: "all their text"}
         """
-        members = [name for name in members]
-        personMessageDict = {name: "" for name in members}
+        personMessageDict = {name: "" for name in self.members}
+        test = []
         for thread in self.threadObjects:
-            if len(set(members) & set(thread.participants)) > 1:
+            if len(set(self.members) & set(thread.participants)) > 1:
+                test.append(thread.participants)
                 thread.aggregateMessages()
                 for message in thread.Messages:
-                    if message.sender in members:
+                    if message.sender in self.members:
                         name = message.sender
                         personMessageDict[name] += ("\n" + message.content)
             else:
                 pass
-        return personMessageDict
+        self.personMessageDict = personMessageDict
+
+    def prepPersonMessageDict(self, *people):
+        for person in people:
+            if person in self.personMessageDict.keys():
+                self.personMessageDict[person] = \
+                        prepString4Nltk(self.personMessageDict[person])
 
 
+    def findTopWords(self, *person):
+        pass
 
 
 if __name__ == "__main__":
-    foo = Archive(stubfile)
+    foo = PeopleArchive(fullfile, "Tyler Wood", "Edmarc Hedrick")
     foo.makeArchiveSoup()
     foo.pullThreads()
-    boo = foo.pullPersonMessages("Tyler Wood", "Edmarc Hedrick")
+    foo.pullPersonMessages()
 
-    for x in boo:
-        print x
+    for k, v in foo.personMessageDict.items():
+        print k, len(v)
 
-#    for k in bar:
-#    for f in k.Messages:
-#            if (f.sender[0] == 'Brent Klauck'):
-#                if len(f.content) > 0:
-#                    print(f.sender, f.content[0])
-#                    print ((f.sender[0] == 'Brent Klauck'))
-#            else:
-#                pass
+    foo.prepPersonMessageDict("Tyler Wood", "Edmarc Hedrick")
 
+    print(len(foo.personMessageDict))
+
+    for k, v in foo.personMessageDict.items():
+        print k, len(v)
 
